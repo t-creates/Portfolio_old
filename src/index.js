4a58cd25b231013678d6bb6e49d3298c7cd85cb8
Travis Geislinger <travisgeislinger@Traviss-MacBook-Pro.local>
2022-04-14 18:42:33 -0600

    Update Projects

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+import 'tachyons';
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
